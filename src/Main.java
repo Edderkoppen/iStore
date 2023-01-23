a158b52a0394e2fd26941fd48b1cab0087b00060
Mathieu <cartron.mathieu37@gmail.com>
2023-01-23 22:45:53 +0100

    Tentative connection BDD
    Manque visiblement un driver. Ne trouve pas "com.mysql.jdbc.Driver"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Victor!");
+        String BDD = "istore";
+        String url = "jdbc:mysql://localhost:3306/istore";
+        String user = "root";
+        String passwd = "";
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, user, passwd);
+            System.out.println("Connecté");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("use istore;");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Impossible de charger le Driver");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }

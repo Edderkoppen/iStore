ba6eac988375892dde4aedd7c7bfc3fd7d6c706a
Mathieu <cartron.mathieu37@gmail.com>
2023-01-24 00:04:06 +0100

    Update: Connection a la BDD
    merci Basile

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,3 +1,5 @@
+package test;
+
 import java.sql.*;
 
 public class Main {
@@ -5,18 +7,20 @@ public class Main {
         String BDD = "istore";
         String url = "jdbc:mysql://localhost:3306/istore";
         String user = "root";
-        String passwd = "";
+        String passwd = "root";
 
         try {
-
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(url, user, passwd);
             System.out.println("Connecté");
 
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("use istore;");
+            ResultSet rs = stmt.executeQuery("select * from user;");
+            rs.next();
+            System.out.println(rs.getString(2));
 
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
             System.out.println("Impossible de charger le Driver");
 
         } catch (SQLException e) {

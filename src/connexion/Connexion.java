0b8348a7f6b3ba6130387d0fae2165b089ed33a2
Mathieu <cartron.mathieu37@gmail.com>
2023-01-25 10:41:45 +0100

    Update: Ajout de la classe de connexion

---
+++ src/connexion/Connexion.java
@@ -0,0 +1,31 @@
+package connexion;
+
+import java.sql.*;
+
+public class Connexion {
+
+    public static void connexionBdd() {
+        String BDD = "istore";
+        String url = "jdbc:mysql://localhost:3306/" + BDD;
+        String user = "root";
+        String passwd = "root";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, user, passwd);
+            System.out.println("Connecté");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from user;");
+            rs.next();
+            System.out.println(rs.getString(2));
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Impossible de charger le Driver");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,31 +1,11 @@
 package test;
 
-import java.sql.*;
+import connexion.Connexion;
+
 
 public class Main {
     public static void main(String[] args) {
-        String BDD = "istore";
-        String url = "jdbc:mysql://localhost:3306/istore";
-        String user = "root";
-        String passwd = "root";
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, user, passwd);
-            System.out.println("Connecté");
-
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from user;");
-            rs.next();
-            System.out.println(rs.getString(2));
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Impossible de charger le Driver");
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+        Connexion connexion = new Connexion();
+        connexion.connexionBdd();
     }
 }

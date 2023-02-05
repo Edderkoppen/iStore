ac5b73afbe97a490e4ad2710254b71f5307e8bdf
KWS0526 <yangv140@gmail.com>
2023-02-05 22:57:50 +0100

    inventory panel

--- src/components/pages/InventoryPanel.java
+++ src/components/pages/InventoryPanel.java
@@ -12,30 +12,34 @@ public class InventoryPanel extends JPanel{
         this.panelW = panelW;
         this.panelH = panelH;
 
-        int widthComponent = 200;
+        int widthComponent = 50;
         int heightComponent = 30;
 
-        JLabel userNameLabel = new JLabel("Utilisateur :");
-        JLabel passwordLabel = new JLabel("Mot de passe :");
 
-        JTextField userNameField = new JTextField();
-        JPasswordField passwordField = new JPasswordField();
+        JLabel imageLabel = new JLabel();
+        imageLabel.setIcon(new ImageIcon("src/assets/icons/copy.png"));
+        JLabel itemNameLabel = new JLabel("AirMax");
+        JLabel priceLabel = new JLabel("190 €");
+        JLabel quantityLabel = new JLabel("1");
 
-        JButton submit = new JButton("Se connecter");
+        JButton increase = new JButton("+");
+        JButton decrease = new JButton("-");
 
-        userNameLabel.setBounds((int) (this.panelW*0.5) - widthComponent/2, (int) (this.panelH*0.25) - heightComponent/2, widthComponent, heightComponent);
-        userNameField.setBounds((int) (this.panelW*0.6) - widthComponent/2, (int) (this.panelH*0.25) - heightComponent/2, widthComponent, heightComponent);
+        //Position
+        imageLabel.setBounds((int) (this.panelW*0.1) - widthComponent/2, (int) (this.panelH*0.5) - heightComponent/2, widthComponent, heightComponent);
+        itemNameLabel.setBounds((int) (this.panelW*0.2) - widthComponent/2, (int) (this.panelH*0.5) - heightComponent/2, widthComponent, heightComponent);
+        priceLabel.setBounds((int) (this.panelW*0.3) - widthComponent/2, (int) (this.panelH*0.5) - heightComponent/2, widthComponent, heightComponent);
+        increase.setBounds((int) (this.panelW*0.4) - widthComponent/2, (int) (this.panelH*0.5) - heightComponent/2, widthComponent, heightComponent);
+        quantityLabel.setBounds((int) (this.panelW*0.5) - widthComponent/2, (int) (this.panelH*0.5) - heightComponent/2, widthComponent, heightComponent);
+        decrease.setBounds((int) (this.panelW*0.6) - widthComponent/2, (int) (this.panelH*0.5) - heightComponent/2, widthComponent, heightComponent);
 
-        passwordLabel.setBounds((int) (this.panelW*0.5) - widthComponent/2, (int) (this.panelH*0.5) - heightComponent/2, widthComponent, heightComponent);
-        passwordField.setBounds((int) (this.panelW*0.6) - widthComponent/2, (int) (this.panelH*0.5) - heightComponent/2, widthComponent, heightComponent);
+        this.add(imageLabel);
+        this.add(itemNameLabel);
+        this.add(priceLabel);
+        this.add(increase);
+        this.add(quantityLabel);
+        this.add(decrease);
 
-        submit.setBounds((int) (this.panelW*0.5) - widthComponent/2, (int) (this.panelH*0.75) - heightComponent/2, widthComponent, heightComponent);
-
-        this.add(userNameLabel);
-        this.add(userNameField);
-        this.add(passwordLabel);
-        this.add(passwordField);
-        this.add(submit);
 
         this.setBackground(Color.white);
     }

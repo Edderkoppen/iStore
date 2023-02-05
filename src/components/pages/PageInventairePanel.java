7109f943c486d42a4e231d5fcd0ebd9af9759c77
Mathieu <cartron.mathieu37@gmail.com>
2023-02-05 19:13:26 +0100

    Creation nouveau panel

---
+++ src/components/pages/PageInventairePanel.java
@@ -0,0 +1,15 @@
+package components.pages;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PageInventairePanel {
+
+    public static JPanel createInventoryPage() {
+        JPanel pan = new JPanel();
+
+        pan.setBackground(Color.MAGENTA);
+
+        return pan;
+    }
+}
